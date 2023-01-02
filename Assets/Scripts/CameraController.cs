f35a090bcf862722790980a098270b3ffcfe24a9
Assanali Amangeldiyev <deiteiai@gmail.com>
2023-01-02 21:28:30 +0800

    Added new input system and camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private const float MIN_FOLLOW_Y_OFFSET = 2f;
+    private const float MAX_FOLLOW_Y_OFFSET = 12f;
+
+    [SerializeField] private CinemachineVirtualCamera cinemachineVirtualCamera;
+    [SerializeField] private float zoomSpeed = 10f;
+    private Vector3 targetFollowOffset;
+    private CinemachineTransposer cinemachineTransposer;
+
+    private void Start()
+    {
+        cinemachineTransposer = cinemachineVirtualCamera.GetCinemachineComponent<CinemachineTransposer>();
+        targetFollowOffset = cinemachineTransposer.m_FollowOffset;
+    }
+
+    private void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+        HandleZoom();
+    }
+    
+    private void HandleMovement()
+    {
+        Vector2 inputMoveDirection = InputManager.Instance.GetCameraMoveAmount();
+
+        float moveSpeed = 15f;
+        Vector3 moveVector = transform.forward * inputMoveDirection.y + transform.right * inputMoveDirection.x;
+        transform.position += moveVector * moveSpeed * Time.deltaTime;
+    }
+
+    private void HandleRotation()
+    {
+        Vector3 rotationVector = new Vector3(0, 0, 0);
+        rotationVector.y = InputManager.Instance.GetCameraRotateAmount();
+
+        float rotationSpeed = 125f;
+        transform.eulerAngles += rotationVector * rotationSpeed * Time.deltaTime;
+    }
+    
+    private void HandleZoom()
+    {
+
+        targetFollowOffset.y += InputManager.Instance.GetCameraZoomAmount();
+
+        targetFollowOffset.y = Mathf.Clamp(targetFollowOffset.y, MIN_FOLLOW_Y_OFFSET, MAX_FOLLOW_Y_OFFSET);
+        cinemachineTransposer.m_FollowOffset = Vector3.Lerp(cinemachineTransposer.m_FollowOffset, targetFollowOffset,
+            Time.deltaTime * zoomSpeed);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    private InputSystem inputSystem;
+    
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Debug.LogError("There's more than one InputManager!" + transform + " - " + Instance);
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        inputSystem = new InputSystem();
+        inputSystem.Player.Enable();
+    }
+
+    public Vector2 GetCameraMoveAmount()
+    {
+        return inputSystem.Player.CameraMovement.ReadValue<Vector2>();
+    }
+
+    public float GetCameraRotateAmount()
+    {
+        return inputSystem.Player.CameraRotation.ReadValue<float>();
+    }
+
+    public float GetCameraZoomAmount()
+    {
+        return inputSystem.Player.CameraZoom.ReadValue<float>();
+    }
+}
